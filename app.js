d75795832323c83b1a5d7f20f24332202656dc78
Anshuman Mishra <anshuman@firehound.org>
2020-08-28 17:49:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ app.get('/~:id', getSnippet);
 app.get('/', home);
 
 
-const PORT = process.env.BIN_PORT || 1998;
+const PORT = process.env.PORT || 1998;
 app.listen(PORT, () => console.log('Listening on port', PORT));
